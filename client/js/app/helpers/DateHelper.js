7618d283f39d1695f08bca60a057be17aa843d87
Denis Missias <denis@socialminer.com>
2019-10-10 17:18:55 -0300

    Improve regex

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,7 +5,7 @@ class DateHelper {
     }
 
     static textToDate(text) {
-        if (!/\d{4}-\d{2}-\d{2}/.test(text)) {
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(text)) {
             throw new Error("Invalid format. Need be aaaa-mm-dd");
         }
         
